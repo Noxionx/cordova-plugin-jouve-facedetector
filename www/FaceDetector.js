3bebebad634b7269deb9c267524686087dad6e4a
fgade <fgade@flatironsjouve.com>
2018-05-23 17:01:53 +0200

    Fixed FaceDetector.js

--- www/FaceDetector.js
+++ www/FaceDetector.js
@@ -4,21 +4,12 @@ var running = false;
 
 var FaceDetector = function() {};
 
-FaceDetector.prototype.start = function(success, fail) {
+FaceDetector.start = function(success, fail) {
     exec(success, fail, PLUGIN_NAME, "start", []);
 };
 
-FaceDetector.prototype.stop = function(success, fail) {
+FaceDetector.stop = function(success, fail) {
     exec(success, fail, PLUGIN_NAME, "stop", []);
 };
 
-if (!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.faceDetector) {
-    window.plugins.facedetector = new FaceDetector();
-}
-
-if (module.exports) {
-    module.exports = FaceDetector;
-}
+module.exports = FaceDetector;
